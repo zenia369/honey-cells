79c0c46419e31fd9ad9c57df050f38ea9b9345a3
Kushnir E. <kyshnirxeneea@gmail.com>
2023-12-28 10:25:51 +0200

    refact: add second page

--- src/pages/playground/1.tsx
+++ src/pages/playground/1.tsx
@@ -11,7 +11,7 @@ import { ChangeEventHandler, useState } from "react";
 import { useQuestContext } from "../../context/Quest.context";
 
 const QuestConfig = {
-  nextRoute: "/playground/1/3",
+  nextRoute: "/playground/1/2",
   steps: [null, "step one"],
   answer: "two",
   task: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,excepturi accusantium veritatis doloremque aliquam id maiores reprehenderit, voluptatum illum ex natus velit enim dolore, blanditiis voluptate inventore porro expedita? Aut!",
--- src/pages/playground/2.tsx
+++ src/pages/playground/2.tsx
@@ -1,12 +1,74 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import Quest from "../../components/Quest";
+import { useQuestContext } from "../../context/Quest.context";
+import { ChangeEventHandler, useState } from "react";
+
+const QuestConfig = {
+  nextRoute: "/playground/1/3",
+  steps: [null, "step one"],
+  answer: "two",
+  task: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,excepturi accusantium veritatis doloremque aliquam id maiores reprehenderit, voluptatum illum ex natus velit enim dolore, blanditiis voluptate inventore porro expedita? Aut!",
+  validator: function <T = string>(value: T): boolean {
+    return value === this.answer;
+  },
+};
+
+const PlaygroundTwoContent = () => {
+  const { validateAnswer, curentStep } = useQuestContext();
+  const [value, setValue] = useState("");
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = () => {
+    validateAnswer(value);
+  };
+
+  return (
+    <>
+      <Divider />
+      <Stack spacing={2}>
+        <Box mt={4}>
+          <Input
+            value={value}
+            onChange={handleChange}
+            placeholder="Вводь значення туть"
+            size="lg"
+            color="white"
+          />
+        </Box>
+        <Button colorScheme="green" onClick={handleClick}>
+          Перевірити!
+        </Button>
+        <Box color="white">{curentStep}</Box>
+      </Stack>
+    </>
+  );
+};
 
 const PlaygroundTwo = () => {
   return (
-    <Container>
-      потребує зміни <br />2 Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Non, excepturi accusantium veritatis doloremque aliquam id maiores
-      reprehenderit, voluptatum illum ex natus velit enim dolore, blanditiis
-      voluptate inventore porro expedita? Aut!
+    <Container color="white">
+      <Quest
+        nextRoute={QuestConfig.nextRoute}
+        task={QuestConfig.task}
+        validator={QuestConfig.validator.bind(QuestConfig)}
+        steps={QuestConfig.steps}
+      >
+        <Box as="p" mb={4} color="white">
+          Note! Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
+          excep velit enim dolore, blanditiis voluptate inventore porro
+          expedita? Aut!
+        </Box>
+        <PlaygroundTwoContent />
+      </Quest>
     </Container>
   );
 };
