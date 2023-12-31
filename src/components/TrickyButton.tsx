529e8299785c0c0835a3ccd2a54d0ebc0252d57d
Kushnir E. <kyshnirxeneea@gmail.com>
2023-12-31 23:45:35 +0200

    feat: add secret key

--- src/components/TrickyButton.tsx
+++ src/components/TrickyButton.tsx
@@ -1,5 +1,5 @@
-import { Button, Tooltip, Box } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Button, Tooltip, Box, Input, Stack } from "@chakra-ui/react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const steps = [
@@ -32,31 +32,33 @@ const steps = [
 const TrickyButton = () => {
   const [userClick, setUserClick] = useState(0);
   const [isDisabled, setIsDisabled] = useState(false);
+  const [value, setValue] = useState("");
   const navigate = useNavigate();
 
   const curentStep = steps[userClick];
-
-  useEffect(() => {
-    if (!userClick) return;
-
-    if (userClick === steps.length) {
-      navigate("/playground/1/1", {
-        state: {
-          fromHome: true,
-        },
-      });
-    }
-  }, [userClick, navigate]);
+  const isLastElement = userClick === steps.length - 1;
 
   return (
-    <Box mt={4} transform={curentStep?.position}>
+    <Stack mt={4} transform={curentStep?.position} direction="column" gap={2}>
       <Tooltip label={curentStep?.text}>
         <Button
           colorScheme="blue"
           variant={isDisabled ? "outline" : "solid"}
           onClick={() => {
+            if (isLastElement) {
+              if (!value.length) return;
+
+              if (value === import.meta.env.VITE_GAME_ONE_SECRET) {
+                navigate("/playground/1/1", {
+                  state: {
+                    fromHome: true,
+                  },
+                });
+              }
+              return;
+            }
             setIsDisabled(true);
-            setUserClick((prev) => (prev === steps.length ? prev : prev + 1));
+            setUserClick((prev) => (isLastElement ? prev : prev + 1));
 
             setTimeout(() => {
               setIsDisabled(false);
@@ -64,10 +66,21 @@ const TrickyButton = () => {
           }}
           isDisabled={isDisabled}
         >
-          Почати
+          {isLastElement ? "Підтвердити" : "Почати"}
         </Button>
       </Tooltip>
-    </Box>
+      {isLastElement ? (
+        <Box mt={4}>
+          <Input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+            placeholder="Секретний ключ водь туть"
+            size="lg"
+            color="white"
+          />
+        </Box>
+      ) : null}
+    </Stack>
   );
 };
 
